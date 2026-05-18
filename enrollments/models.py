04aa8c70cbe6931288203b164e063aeff45d74a8
Pheane <tselisto@yahoo.com>
2026-05-18 10:36:21 +0200

    Fix settings.py configuration

--- enrollments/models.py
+++ enrollments/models.py
@@ -113,3 +113,5 @@ class Enrollment(models.Model):
         return f"{self.student} - {self.course}"
 
 '''
+from django.core.management.utils import get_random_secret_key
+print(get_random_secret_key())
