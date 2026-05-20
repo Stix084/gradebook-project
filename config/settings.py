a4ca58d5dfa8481b0a9cb85f35d360e1064732b5
Pheane <tselisto@yahoo.com>
2026-05-20 22:31:31 +0200

    Added whitenoise dependency

--- config/settings.py
+++ config/settings.py
@@ -29,13 +29,13 @@ CSRF_TRUSTED_ORIGINS = [
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = [
     "gradebook-project-p7tb.onrender.com",
     "localhost",
     "127.0.0.1",
-]
+]   
 
 
 # Application definition
