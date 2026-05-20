5874a1e76700b4e499a5d7c89de67d270db79f15
Pheane <tselisto@yahoo.com>
2026-05-21 00:11:33 +0200

    Fix course_detail URL routing

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from courses.models import Course
-from users.views import student_dashboard, course_detail
-
 from users.views import (
     course_detail,
     role_redirect,
@@ -14,20 +9,9 @@ from users.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
     path("accounts/", include("django.contrib.auth.urls")),
-
     path("", role_redirect, name="home"),
-
     path("dashboard/", student_dashboard, name="dashboard"),
-    
-
-    path(
-        "lecturer-dashboard/",
-        lecturer_dashboard,
-        name="lecturer_dashboard"
-    ),
-     path("course/<int:id>/", course_detail, name="course_detail"),
-
-     
+    path("lecturer-dashboard/", lecturer_dashboard, name="lecturer_dashboard"),
+    path("course/<int:id>/", course_detail, name="course_detail"),
 ]
