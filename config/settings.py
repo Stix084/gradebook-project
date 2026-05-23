4f9d01b091fcd19f31394517e579564d2ccb7f49
Pheane <tselisto@yahoo.com>
2026-05-23 14:13:09 +0200

    add custom error pages and set DEBUG=False

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ CSRF_TRUSTED_ORIGINS = [
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = [
     "gradebook-project-p7tb.onrender.com",
