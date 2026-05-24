60dd7dd0eb1777619fc0523adfe2ade7ec2f07c6
Pheane <tselisto@yahoo.com>
2026-05-24 04:40:08 +0200

    add lecturer field to CourseAdmin

--- courses/admin.py
+++ courses/admin.py
@@ -4,14 +4,15 @@ from .models import Course, Assessment, Grade
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("code", "name")
+    list_display = ("code", "name", "lecturer")
     search_fields = ("code", "name")
+    list_filter = ("lecturer",)
 
 
 @admin.register(Assessment)
 class AssessmentAdmin(admin.ModelAdmin):
-    list_display = ("course", "title", "total_marks", "weight")
-    list_filter = ("course",)
+    list_display = ("course", "title", "assessment_type", "total_marks", "weight")
+    list_filter = ("course", "assessment_type")
     search_fields = ("title",)
 
 
@@ -19,10 +20,8 @@ class AssessmentAdmin(admin.ModelAdmin):
 class GradeAdmin(admin.ModelAdmin):
     list_display = ("student", "assessment", "marks_obtained")
     list_filter = ("assessment",)
-
     actions = ["reset_grades"]
 
     def reset_grades(self, request, queryset):
         queryset.update(marks_obtained=0)
-
     reset_grades.short_description = "Reset selected grades"
